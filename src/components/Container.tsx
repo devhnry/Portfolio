8ca67fe5a84932493d34e7417cbbcca6032253c0
devhnry <devthenry@gmail.com>
2025-01-11 22:11:01 +0100

    style: updated Container component to take custom classes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,11 +2,12 @@ import React, {ReactNode} from 'react'
 
 interface ContainerProps {
 	children: ReactNode
+	classes?: string
 }
 
-export const Container: React.FC<ContainerProps> = ({children}) => {
+export const Container: React.FC<ContainerProps> = ({children, classes}) => {
 	return (
-		<main className={`max-w-[1440px] px-6 w-full mx-auto`}>
+		<main className={`max-w-[1440px] px-6 w-full mx-auto overscroll-none ${classes ? `!${classes}` : ''}`}>
 			{children}
 		</main>
 	)
