ec4e73c88ca66f931f58ad61f07a708f5cea663f
devhnry <devthenry@gmail.com>
2025-01-13 20:09:50 +0100

    style: updated the grainOverlay texture

--- src/components/GrainOverlay.tsx
+++ src/components/GrainOverlay.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import {useEffect, useState} from "react"
 
 export function GrainOverlay() {
 	const [isLoaded, setIsLoaded] = useState(false)
@@ -8,14 +8,14 @@ export function GrainOverlay() {
 	}, [])
 
 	return (
-		<div
-			className={`pointer-events-none fixed inset-0 z-50 mix-blend-multiply
-        [background-image:url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMDAiIGhlaWdodD0iMzAwIj48ZmlsdGVyIGlkPSJhIiB4PSIwIiB5PSIwIj48ZmVUdXJidWxlbmNlIHR5cGU9ImZyYWN0YWxOb2lzZSIgYmFzZUZyZXF1ZW5jeT0iMC45IiBzdGl0Y2hUaWxlcz0ic3RpdGNoIi8+PGZlQ29sb3JNYXRyaXggdHlwZT0ic2F0dXJhdGUiIHZhbHVlcz0iMCIvPjwvZmlsdGVyPjxyZWN0IHdpZHRoPSIzMDAiIGhlaWdodD0iMzAwIiBmaWx0ZXI9InVybCgjYSkiIG9wYWNpdHk9IjAuMzc1Ii8+PC9zdmc+')]
-        after:content-[''] after:absolute after:inset-0 after:bg-repeat after:opacity-10 after:mix-blend-overlay
-        before:content-[''] before:absolute before:inset-0 before:bg-[#0f0f0f] before:opacity-[0.1] before:mix-blend-darken
+			<div
+					className={`pointer-events-none fixed inset-0 z-50 mix-blend-screen opacity-[0.07]
+        [background-image:url('https://i.ibb.co/FhfBXMD/static.jpg')]
+        after:content-[''] after:absolute after:inset-0 after:bg-repeat after:opacity-[0.02] after:mix-blend-screen
+        before:content-[''] before:absolute before:inset-0 before:bg-[#000000] before:opacity-[0.02] before:mix-blend-screen
         ${isLoaded ? "opacity-100" : "opacity-0"}
-        transition-opacity duration-1000`}
-			aria-hidden="true"
-		/>
+        transition-opacity duration-1000 z-[99999]`}
+					aria-hidden="true"
+			/>
 	)
 }
