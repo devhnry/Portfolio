fc3101b494a87eeccb207155774dfe8ddc5eaa88
devhnry <devthenry@gmail.com>
2025-01-25 00:38:48 +0100

    refactor: updated the navbar styles and removed nav items

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import {SectionMobile} from "./components/HeroSection.tsx";
 import backgroundImage from "./assets/hero-image.webp";
+import {Navbar} from "./components/Navbar.tsx";
 
 
 const App = () => {
 
 		return (
-				<main style={{ backgroundImage: `url(${backgroundImage})`}} className={`bg-no-repeat bg-cover bg-center h-dvh`}>
+				<main style={{ backgroundImage: `url(${backgroundImage})`}} className={`bg-no-repeat relative bg-cover bg-center h-dvh`}>
+						<Navbar />
 						<SectionMobile />
 				</main>
 		)
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,23 @@
-import {motion as m} from "motion/react"
-import {useState} from "react";
-
 export const Navbar = () => {
-		const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
-		const navItems: string[] = ["services", "works", "about", "experience", "contact"]
 
 		return (
-				<header className={`font-inter capitalize text-md text-paragraph-1 md:text-[1.0625rem]`}>
-						<nav className={`flex justify-between items-start md:items-center`}>
+				<header className={`sticky top-0 capitalize text-beige-light-200 mx-6 pt-4`}>
+						<nav className={`flex justify-between items-center md:items-center`}>
 								<div className={`md:flex gap-x-12`}>
-										<p className={`font-bold text-black relative w-fit`}>By Henry
+										<p className={`font-semibold  relative w-fit capitalize`}>byhenry
 												<span className={`absolute -right-[10px] top-0`}>
-						<img className={`size-[10px]`} src={"./src/assets/copyright.svg"} alt={``}/>
-						</span>
+														<img className={`size-[10px]`} src={"./src/assets/copyright.svg"} alt={``}/>
+												</span>
 										</p>
-										<p className={`font-light text-paragraph-1 text-sm xsl:text-[1.05rem] md:text-[clamp(0.9375rem,2.1vw,1.125rem)]`}>(Software
-																																																																			Engineer)</p>
+										<p className={`font-light text-paragraph-1 text-[12px]`}>
+												(FullStack Engineer)
+										</p>
 								</div>
-								<ul className={`flex flex-col gap-y-[0.3px] md:flex-row items-start md:items-center gap-x-2 sm:text-[17px] font-light leading-[1.35] md:text-[clamp(0.9375rem,2.1vw,1.065rem)] overflow-hidden`}>
-										{navItems.map((item, i) => {
-												return (
-														<li key={item} className="relative overflow-hidden flex cursor-none">
-																<div className="block h-[28px] overflow-hidden"
-																		 onMouseEnter={() => setHoveredIndex(i)}
-																		 onMouseLeave={() => setHoveredIndex(null)}>
-																		<m.div
-																				className="flex flex-col gap-y-2 items-center"
-																				animate={{y: hoveredIndex === i ? '-56%' : '0%'}}
-																				transition={{
-																						duration: 0.3,
-																						ease: [0.4, 0, 0.2, 1],
-																				}}
-																		>
-																				<span>{item}</span>
-																				<m.span initial={{y: '0%'}}
-																								animate={{opacity: hoveredIndex === i ? 1 : 0.5}}>{item}</m.span>
-																		</m.div>
-																</div>
-																<span>,</span>
-														</li>
-												)
-										})}
-								</ul>
+								<div className={`w-fit flex flex-col gap-y-1`}>
+										<div className={`bg-beige-light-400 w-[30px] h-[3px] ml-auto`}></div>
+										<div className={`bg-beige-light-400 w-[50px] h-[3px]`}></div>
+										<div className={`bg-beige-light-400 w-[30px] h-[3px]`}></div>
+								</div>
 						</nav>
 				</header>
 		)
