13c838a8574d13ae5ead85c30af2eb2a5b2fce9e
devhnry <devthenry@gmail.com>
2025-01-25 01:08:12 +0100

    style: included a minimal animation on an img on the hero section

--- src/components/GrainOverlay.tsx
+++ src/components/GrainOverlay.tsx
@@ -9,10 +9,10 @@ export function GrainOverlay() {
 
 	return (
 			<div
-					className={`pointer-events-none fixed inset-0 z-50 mix-blend-screen opacity-[0.07]
+					className={`pointer-events-none fixed inset-0 z-50 mix-blend-screen opacity-[0.01]
         [background-image:url('https://i.ibb.co/FhfBXMD/static.jpg')]
-        after:content-[''] after:absolute after:inset-0 after:bg-repeat after:opacity-[0.02] after:mix-blend-screen
-        before:content-[''] before:absolute before:inset-0 before:bg-[#000000] before:opacity-[0.02] before:mix-blend-screen
+        after:content-[''] after:absolute after:inset-0 after:bg-repeat after:opacity-[0.01] after:mix-blend-screen
+        before:content-[''] before:absolute before:inset-0 before:bg-[#000000] before:opacity-[0.01] before:mix-blend-screen
         ${isLoaded ? "opacity-100" : "opacity-0"}
         transition-opacity duration-1000 z-[99999]`}
 					aria-hidden="true"
